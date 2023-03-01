cedfd48ffd6bfe96ae0c96c10c891b358ce496c6
TheFieryFalcon <72846259+TheFieryFalcon@users.noreply.github.com>
2023-03-01 16:33:05 +0800

    Removed (unused) localization features

--- src/main/java/com/gildedgames/the_aether/client/gui/trivia/AetherTrivia.java
+++ src/main/java/com/gildedgames/the_aether/client/gui/trivia/AetherTrivia.java
@@ -7,77 +7,48 @@ import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Random;
-
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.resources.I18n;
 import net.minecraft.client.resources.IResource;
 import net.minecraft.util.ResourceLocation;
-
-import net.minecraft.util.text.TextComponentTranslation;
 import org.apache.commons.io.IOUtils;
-
 import com.google.common.collect.Lists;
-
 public class AetherTrivia
 {
-
     private static Random random = new Random();
-
     public AetherTrivia()
     {
     	
     }
-
     public static String getNewTrivia() 
     {
-        String localization = Minecraft.getMinecraft().getLanguageManager().getCurrentLanguage().getLanguageCode();
-
-        if (getEntriesForLocalization(localization) != null)
-        {
-            return getEntriesForLocalization(localization);
-        }
-        else if (getEntriesForLocalization("en_us") != null)
-        {
-            return getEntriesForLocalization("en_us");
-        }
-        else
-        {
-            return "missingno";
-        }
-    }
-
-    public static String getEntriesForLocalization(String localization)
-    {
         IResource iresource = null;
-
         try
         {
-            List<String> list = Lists.newArrayList();
-            iresource = Minecraft.getMinecraft().getResourceManager().getResource(new ResourceLocation("aether_legacy", "texts/" + localization + ".txt"));
+            List<String> list = Lists.<String>newArrayList();
+            iresource = Minecraft.getMinecraft().getResourceManager().getResource(new ResourceLocation("aether_legacy", "texts/trivia.txt"));
             BufferedReader bufferedreader = new BufferedReader(new InputStreamReader(iresource.getInputStream(), StandardCharsets.UTF_8));
             String s;
-
             while ((s = bufferedreader.readLine()) != null)
             {
                 s = s.trim();
-
                 if (!s.isEmpty())
                 {
                     list.add(s);
                 }
             }
-
             if (!list.isEmpty())
             {
-                return I18n.format("gui.aether_trivia.pro_tip") + " " + list.get(random.nextInt(list.size()));
+            	return "Pro Tip: " + (String)list.get(random.nextInt(list.size()));
             }
         }
-        catch (IOException ignore) { }
+        catch (IOException var8)
+        {
+            ;
+        }
         finally
         {
-            IOUtils.closeQuietly(iresource);
+            IOUtils.closeQuietly((Closeable)iresource);
         }
-
-        return null;
+        return "missingno";
     }
 }
